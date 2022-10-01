8623b3271ce7fcf3fce630041245c638c5f7befc
NestorMartire <nestorm1979@hotmail.com>
2022-10-01 12:33:22 -0300

    first commit

---
+++ JS_Ejercicio2.js
@@ -0,0 +1,8 @@
+const obj = {
+    Titulo: "No entiendo nada de JavaScript",
+    Autor: "Nestor Martire",
+    Fecha: 01/10/2022,
+    URL: "www.noentiendo.com",
+}
+
+const lista = ["NestorMartire", 43, true, 10/07/1979, obj]
